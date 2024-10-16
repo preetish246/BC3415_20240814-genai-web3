7679b01ef9be7afeb352ff2fd3b859a8978c3832
preetish246 <104291737+preetish246@users.noreply.github.com>
2024-10-16 09:00:28 +0000

    smart contract

--- app.py
+++ app.py
@@ -67,5 +67,9 @@ def generate_jokes_when_api_is_down():
     ]
     return random.choice(jokes)
 
+@app.route("/paynow", methods=["GET", "POST"])
+def paynow():
+    return render_template("paynow.html")
+
 if __name__ == "__main__":
     app.run()
