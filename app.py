b1297e8532d702f6894bd97cb98d342ed9e5aec4
preetish246 <104291737+preetish246@users.noreply.github.com>
2024-11-10 07:33:51 +0000

    Latest

--- app.py
+++ app.py
@@ -1,47 +1,32 @@
-from flask import Flask, render_template, request
-import google.generativeai as palm
-import google.api_core.exceptions as google_exceptions
+from flask import Flask,render_template,request
+import google.generativeai as genai
 import os
-import random
+import textblob
 
-# Configure the API key
-api = os.environ.get('GOOGLE_KEY')
-palm.configure(api_key=api)
-
-# Correct model name (replace with the model name you have access to)
-model_name = "models/text-bison-001"  # Make sure this is the correct and accessible model name
+#os.environ["MAKERSUITE_API_KEY] = ""
+api = "AIzaSyCFIL-2qRWHrUqzyf_TN3A5IKQsVgB2zHg"
+genai.configure(api_key=api)
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"]) 
+@app.route("/", methods=["GET","POST"])
 def index():
-    return render_template('index.html')
+    return(render_template("index.html"))
 
-@app.route("/ai_agent", methods=["GET", "POST"])
+@app.route("/ai_agent", methods=["GET","POST"])
 def ai_agent():
-    return render_template("ai_agent.html")
+    q = "Tell me a joke about Singapore"
+    r = model.generate_content(q)
+    return(render_template("ai_agent_reply.html",r=r.text))
 
-@app.route("/ai_agent_reply", methods=["GET", "POST"])
-def ai_agent_reply():
-    q = request.form.get("q")
-    try:
-        response = palm.generate_text(
-            model=model_name,
-            prompt=q,
-            temperature=0.7,
-            max_output_tokens=200
-        )
-        return render_template("ai_agent_reply.html", r=response.result)
-    except google_exceptions.InvalidArgument as e:
-        return f"Error: {str(e)}"
-
-@app.route("/prediction", methods=["GET", "POST"])
+@app.route("/prediction", methods=["GET","POST"])
 def prediction():
-    return render_template("index.html")
+    return(render_template("index.html"))
 
-@app.route("/paynow", methods=["GET", "POST"])
+@app.route("/paynow", methods=["GET","POST"])
 def paynow():
-    return render_template("paynow.html")
+    return(render_template("paynow.html"))
 
 @app.route("/SA", methods=["GET","POST"])
 def SA():
@@ -52,7 +37,6 @@ def SA_reply():
     q = request.form.get("q")
     r = textblob.TextBlob(q).sentiment
     return(render_template("SA_reply.html",r=r))
-
 
 if __name__ == "__main__":
     app.run()
